17123dbe50a17d4cd145c00f685012d20351f1f0
johnlombardi389 <lombardij38@gmail.com>
2024-07-21 16:43:58 -0400

    Added social media links and contact to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,32 @@
 // Style
 import styled from "styled-components";
+// Icons
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <StyledFooter>
       <div>
         <p>Website designed and coded by John Lombardi (That's me again!)</p>
+        <IconContainer>
+          <a
+            href="https://www.linkedin.com/in/johnlombardi389/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/johnlombardi389"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+          <a href="mailto:johnlombardi389@gmail.com">
+            <FaEnvelope />
+          </a>
+        </IconContainer>
       </div>
     </StyledFooter>
   );
@@ -21,4 +42,25 @@ const StyledFooter = styled.footer`
   font-family: "Open Sans", sans-serif;
   text-align: center;
   padding: 2rem 0;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+
+  a {
+    color: #fff;
+    margin: 0 1rem;
+    transition: color 0.3s;
+
+    &:hover {
+      color: #ff7761;
+    }
+  }
+
+  svg {
+    width: 2rem;
+    height: 2rem;
+  }
 `;
